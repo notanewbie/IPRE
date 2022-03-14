431268478b83eb6a728ffd164936174f3835b9c5
notanewbie <computerchipcity@aol.com>
2022-03-14 13:07:14 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+site = {
+"name": "StreamLive Audio",
+"logo": "https://radio.ntnwb.cc/radio.png",
+"channels": "amarschannels.js",
+"about": "Listen to live internet radio for free, with as few advertisements as possible. Brought to you by notanewbie.",
+"ipre": "IPRRE.js",
+"fbimg": "https://static.thenounproject.com/png/433087-200.png",
+"homeimg": "home.png",
+"exploreimg": "explore.png",
+"searchimg": "search.png",
+"npimg": "np.png",
+"playimg": "play.png",
+"pauseimg": "stop.png",
+"stopimg": "stop.png",
+}
