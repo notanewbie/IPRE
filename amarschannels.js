cbe3714089a3cc38bde063c5aa80751e26fc98fe
notanewbie <computerchipcity@aol.com>
2024-07-10 08:56:02 -0400

    Update amarschannels.js

--- amarschannels.js
+++ amarschannels.js
@@ -796,5 +796,7 @@ new Channel("Dateline 24/7", "https://cdn-profiles.tunein.com/s321394/images/log
 new Channel("Nosey", "https://cdn-profiles.tunein.com/s323291/images/logog.jpg", "https://tunein.cdnstream1.com/3658_96.mp3", "Special Interest", "", "live", ""),
 new Channel("Judge Nosey", "https://cdn-profiles.tunein.com/s323324/images/logog.jpg", "https://tunein.cdnstream1.com/3657_96.mp3", "Special Interest", "", "live", ""),
 new Channel("Newstalk 105.9 FM WMAL", "https://cdn-profiles.tunein.com/s30062/images/logog.png", "https://playerservices.streamtheworld.com/api/livestream-redirect/WMALFM.mp3", "Conservative Talk", "", "live", ""),
-new Channel("Red State Talk Radio", "https://cdn-profiles.tunein.com/s123330/images/logog.png", "https://s2.radio.co/s572ad25f7/listen", "Conservative Talk", "", "live", "")
+new Channel("Red State Talk Radio", "https://cdn-profiles.tunein.com/s123330/images/logog.png", "https://s2.radio.co/s572ad25f7/listen", "Conservative Talk", "", "live", ""),
+new Channel("Worship 24/7", "https://worship247.com/wp-content/uploads/2023/05/Worship247-Logo-Square-FullColor.png", "https://worship247.streamguys1.com/live-aac-web", "Worship", "", "live", ""),
+new Channel("Power Of Worship Radio", "https://play-lh.googleusercontent.com/2cjloL4AOWiLM7kWavRAe7uZkgqbhS-rIagie2vDQbj4gcXOHn2Q8gwYAwP60S00TU5N=w750-h480-rw", "https://fwd.autopo.st/powerofworshipradio", "Worship, Gospel, Sermons", "", "live", "")
 ]
